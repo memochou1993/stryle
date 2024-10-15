e4b78a66d754b6e6c9d2f8381569f523d5f00c41
William Chou <williamchou@kklab.com>
2024-10-16 01:35:25 +0800

    Refactor and add Converter class

--- lib/toTitleCase.test.ts
+++ lib/toTitleCase.test.ts
@@ -1,20 +1,41 @@
+import { describe } from 'node:test';
 import { expect, test } from 'vitest';
 import toTitleCase from './toTitleCase';
 
-test('toTitleCase', () => {
-  const cases = [
-    ['hello, world!', 'Hello, World!'],
-    ['snake_case', 'Snake Case'],
-    ['camelCase', 'Camel Case'],
-    ['CamelCase', 'Camel Case'],
-    ['HTMLElement', 'HTML Element'],
-    ['This is an HTML element', 'This is an HTML Element'],
-    ['Is this an HTML element', 'Is This an HTML Element'],
-    ['step-by-step', 'Step-by-Step'],
-  ];
-  
-  cases.forEach(([input, expected]) => {
-    const actual = toTitleCase(input);
-    expect(actual).toBe(expected);
+describe('toTitleCase', () => {
+  test('should convert correctly', () => {
+    const cases = [
+      ['hello, world!', 'Hello, World!'],
+      ['snake_case', 'Snake Case'],
+      ['camelCase', 'Camel Case'],
+      ['CamelCase', 'Camel Case'],
+      ['HTMLElement', 'HTML Element'],
+      ['This is an HTML element', 'This is an HTML Element'],
+      ['Is this an HTML element', 'Is This an HTML Element'],
+      ['step-by-step', 'Step-by-Step'],
+    ];
+
+    cases.forEach(([input, expected]) => {
+      const actual = toTitleCase(input);
+      expect(actual).toBe(expected);
+    });
+  });
+
+  test('should convert with special words', () => {
+    const specialWords = ['iPhone', 'iOS'];
+
+    const cases = [
+      ['This is an iphone', 'This is an iPhone'],
+      ['This is an IPHONE', 'This is an iPhone'],
+      ['This is an iPhone', 'This is an iPhone'],
+      ['This is an ios app', 'This is an iOS App'],
+      ['This is an IOS app', 'This is an iOS App'],
+      ['This is an iOS app', 'This is an iOS App'],
+    ];
+
+    cases.forEach(([input, expected]) => {
+      const actual = toTitleCase(input, { specialWords });
+      expect(actual).toBe(expected);
+    });
   });
 });
--- lib/toTitleCase.ts
+++ lib/toTitleCase.ts
@@ -1,28 +1,73 @@
 import capitalize from './capitalize';
 
-const exceptions = /^(a|an|and|as|at|but|by|for|if|in|is|nor|of|on|or|the|to|with)$/i;
+const EXCEPTIONS = /^(a|an|and|as|at|but|by|for|if|in|is|nor|of|on|or|the|to|with)$/i;
+const PLACEHOLDER = '###PLACEHOLDER###';
 
-const toTitleCase = (str: string): string => {
-  return str
-    .replace(/[_]/g, ' ')
-    .replace(/([a-z])([A-Z])/g, '$1 $2')
-    .replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2')
-    .split(' ')
-    .map((word, index) => processWord(word, index === 0))
-    .join(' ');
-};
+interface ConverterOptions {
+  specialWords?: string[];
+}
 
-const processWord = (word: string, isFirstWord: boolean): string => {
-  if (word === word.toUpperCase()) return word;
-  if (word.includes('-')) {
-    return word.split('-')
-      .map((part, i) => processWord(part, i === 0))
-      .join('-');
+class Converter {
+  private str: string;
+
+  private specialWords: string[];
+
+  constructor(str: string, options: ConverterOptions = {}) {
+    this.str = str;
+    this.specialWords = options.specialWords ?? [];
   }
-  if (isFirstWord || !exceptions.test(word)) {
-    return capitalize(word);
+
+  public convert(): string {
+    return this.encodeSpecialWords()
+      .transform()
+      .decodeSpecialWords()
+      .getResult();
   }
-  return word.toLowerCase();
-};
-  
+
+  private encodeSpecialWords(): this {
+    if (this.specialWords.length < 1) return this;
+    const pattern = new RegExp(this.specialWords.join('|'), 'gi');
+    this.str = this.str.replace(pattern, match => `${PLACEHOLDER}${match.toUpperCase()}${PLACEHOLDER}`);
+    return this;
+  }
+
+  private transform(): this {
+    this.str = this.str
+      .replace(/[_]/g, ' ')
+      .replace(/([a-z])([A-Z])/g, '$1 $2')
+      .replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2')
+      .split(' ')
+      .map((word, index) => this.processWord(word, index === 0))
+      .join(' ');
+    return this;
+  }
+
+  private decodeSpecialWords(): this {
+    if (this.specialWords.length < 1) return this;
+    this.str = this.str.replace(new RegExp(`${PLACEHOLDER}(.+?)${PLACEHOLDER}`, 'g'), (_, match) => {
+      return this.specialWords.find(word => word.toUpperCase() === match.toUpperCase()) || match;
+    });
+    return this;
+  }
+
+  private getResult(): string {
+    return this.str;
+  }
+
+  private processWord(word: string, isFirstWord: boolean): string {
+    if (word === word.toUpperCase()) return word;
+    if (word.includes('-')) {
+      return word.split('-')
+        .map((part, i) => this.processWord(part, i === 0))
+        .join('-');
+    }
+    if (isFirstWord || !EXCEPTIONS.test(word)) {
+      return capitalize(word);
+    }
+    return word.toLowerCase();
+  }
+}
+
+const toTitleCase = (str: string, options: ConverterOptions = {}): string => new Converter(str, options).convert();
+
 export default toTitleCase;
